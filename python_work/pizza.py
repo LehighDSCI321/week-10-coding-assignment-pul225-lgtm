265cb41e50f7fe684c44125e671ee03138a2a4b0
pul225-lgtm <pul225@lehigh.edu>
2025-11-01 18:58:05 -0400

    remove incorrect files

--- python_work/pizza.py
+++
@@ -1,6 +0,0 @@
-
-def make_pizza(size, *toppings):
-    """describe the pizza which will be made"""
-    print(f"\nmaking a {size} pizza")
-    for topping in toppings:
-        print(f"- {topping}")
--- python_work/tuple_case.py
+++
@@ -1,3 +0,0 @@
-dims = (200, 50)
-dims = (10, 100)
-print(dims[0])
