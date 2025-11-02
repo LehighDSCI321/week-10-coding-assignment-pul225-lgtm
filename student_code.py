69c8236536647480f79ea393a2eb49596e852fe4
pul225-lgtm <pul225@lehigh.edu>
2025-11-01 23:13:58 -0400

    Add topological sorting method to SortableDigraph
    Implement Kahn's algorithm for topological sorting in SortableDigraph.

--- student_code.py
+++ student_code.py
@@ -1,3 +1,25 @@
+class SortableDigraph(VersatileDigraph):
+    '''return a topologically sorted list of nodes using Kahn's algorithm'''
+    def top_sort(self):
+        """return a topologically sorted list list of nodes in the graph"""
+        if not self.nodes:
+            return []
+        in_degree_map = {node: 0 for node in self.nodes}
+        for _, ends in self.edges.items():
+            for end in ends:
+                in_degree_map[end] += 1
+        queue = deque([node for node in in_degree_map if in_degree_map[node] == 0])
+        sorted_list = []
+        while queue:
+            current = queue.popleft()
+            sorted_list.append(current)
+            for successor in self.successors(current):
+                in_degree_map[successor] -= 1
+                if in_degree_map[successor] == 0:
+                    queue.append(successor)
+        if len(sorted_list) != len(self.nodes):
+            raise ValueError("graph contains a cycle")
+        return sorted_list
 class TraversableDigraph(SortableDigraph):
     def dfs(self, start):
         """Depth-first search traversal from the start node"""
